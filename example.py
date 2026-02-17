204016c1bdd3a695d031d9d70902961d2102fb86
praveshgrewal <gurahpravesh@gmail.com>
2026-02-17 11:51:57 +0530

    moduler programming

---
+++ example.py
@@ -0,0 +1,14 @@
+def factorial(n):
+    if n ==0 or n ==1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+# factorial(5)
+
+
+def add(a,b):
+    return a +b 
+
+def sub(a,b):
+    return a - b
---
+++ module.py
@@ -0,0 +1,5 @@
+def greet():
+    return "welcome in python"
+
+def add(a,b):
+    return a+ b
